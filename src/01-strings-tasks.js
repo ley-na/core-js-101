8fa816b2eea6806495e102a32e8eca8236165ba8
Leyna <elena-voytovich@yandex.ru>
2023-09-03 16:07:48 +0300

    refactor: code refactoring

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -202,8 +202,8 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(width, height) {
-
+function getRectangleString(/* width, height */) {
+  throw new Error('Not implemented');
 }
 
 
